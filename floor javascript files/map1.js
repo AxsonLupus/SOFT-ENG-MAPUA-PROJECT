5c0cd829dba77a51bcd96c698bb4ecc2e1541da0
V1enna <140500097+V1enna@users.noreply.github.com>
2024-04-22 13:41:01 +0800

    Update map1.js

--- floor javascript files/map1.js
+++ floor javascript files/map1.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var backgroundImage = new Image();
 
   // Set the source of the image
-  backgroundImage.src = "floor1.jpg"; // Replace with the actual path to your image
+  backgroundImage.src = "pic1.png"; // Replace with the actual path to your image
 
   // Once the image is loaded, draw it on the canvas
     backgroundImage.onload = function () {
@@ -50,9 +50,9 @@ class Graph {
 
       while (queue.length > 0) {
           const path = queue.shift();
-          const currentVertex = path[path.length - 1];
+          const currentVertex = path[path.length - 1]; //reduces the path.length by one for every node passed
 
-          if (visited.has(currentVertex.id)) {
+          if (visited.has(currentVertex.id)) { 
               continue;
           }
 
@@ -81,118 +81,110 @@ class Graph {
   
   function findShortestPath() {
 
-    startVertex = document.getElementById('initial').value;
-    endVertex = document.getElementById('final').value;
+    startVertex = document.getElementById('initialLocationBookmark').value;
+    endVertex = document.getElementById('desiredLocationBookmark').value;
     var backgroundImage = new Image();
-    backgroundImage.src = "floor1.jpg"; 
+    backgroundImage.src = "pic1.png"; 
 
-    //console.log(startVertex)
+    //NODES COORDINATES
     const graph = new Graph();
-  graph.addVertex('elevator', 455, 365) //elevator
-  graph.addVertex('fire-exit-elevator', 410, 305) //fire-exit-elevator
-  graph.addVertex('canteen', 230, 290) //canteen
-  graph.addVertex('Student lounge 1', 470, 195) //Student lounge 1
-  graph.addVertex('hallway', 590, 275) //hallway
-  graph.addVertex('clinic', 700, 245) //clinic
-  graph.addVertex('orientation room', 785, 260) //orientation room
-  graph.addVertex('counsellor', 920, 275) //counsellor
-  graph.addVertex('multi-purpose', 945, 280) //multi-purpose
-  graph.addVertex('tech serve', 990, 290) //tech serve
-  graph.addVertex('student council', 785, 310) //student council
-  graph.addVertex('SOCIP', 820, 310) //SOCIP
-  graph.addVertex('Registrar', 790, 365) //Registrar
-  graph.addVertex('admission', 840, 410) //admission
-  graph.addVertex('treasury', 840, 470) //treasury
-  graph.addVertex('student lounge 2', 700, 360) // student lounge 2
-  graph.addVertex('waiting area', 785, 440) // waiting area
-  graph.addVertex('fire exit 2', 640, 400) //fire exit 2
-  graph.addVertex('lobby area', 640, 440) //lobby area
-  graph.addVertex('bookstore',370 ,515 ) //bookstore
-  graph.addVertex('1', 455, 435) //1
-  graph.addVertex('2', 455, 305) //2
-  graph.addVertex('3', 455, 275) //3
-  graph.addVertex('4', 350 ,275) //4
-  graph.addVertex('5', 350, 195) //5
-  graph.addVertex('6',700, 275) //6
-  graph.addVertex('7',785, 275) //7
-  graph.addVertex('8',845 , 285) //8
-  graph.addVertex('9', 920, 285) //9
-  graph.addVertex('10', 945, 300) //10
-  graph.addVertex('11',990, 300) //11
-  graph.addVertex('12', 820, 275) //12
-  graph.addVertex('13', 845, 440) //13
-  graph.addVertex('14', 640, 550 ) //14
-  graph.addVertex('15', 455, 550 ) //15
-  graph.addVertex('16',370 , 550 )//16
-  graph.addVertex('17',845 , 365 )//17
-  graph.addVertex('18',700, 440)//18
-  graph.addVertex('19',640, 435)//19
-  graph.addVertex('20',820, 290)//20
+  graph.addVertex('Elevator Lobby', 302, 342) //elevator
+  graph.addVertex('Canteen', 200, 235)
+  graph.addVertex('Book Store', 110, 410)
+  graph.addVertex('Admin Assist', 965, 341)
+  graph.addVertex('Admin Office', 965, 400)
+  graph.addVertex('Clinic', 250, 382)
+  graph.addVertex('Lobby Area', 385,408)
+  graph.addVertex('Student Lounge 1', 569, 331)
+  graph.addVertex('Student Lounge 2', 302, 235)
+  graph.addVertex('Hallway', 415, 235)
+  graph.addVertex('Orientation Room', 881, 194)
+  graph.addVertex('Nursing Lab', 736,177)
+  graph.addVertex('Student Council', 675, 290)
+  graph.addVertex('SOCIP', 832, 290)
+  graph.addVertex('Registrar', 767, 370)
+  graph.addVertex('Treasury', 852, 408)
+  graph.addVertex('Waiting Area', 767, 408)
+  graph.addVertex('Admission', 852, 370)
+  graph.addVertex('Multi Purpose', 993, 194)
+  graph.addVertex('Tech Room', 1030, 194)
+  
+  
+  
+  //Outside Rooms
+  graph.addVertex('Admin_Out1', 936, 296)
+  graph.addVertex('Admin_Out2', 963, 296)
+  graph.addVertex('Admin_Out3', 963, 235)
+  graph.addVertex('Admin Assist_Out', 936, 341)
+  graph.addVertex('Admin Office_Out', 936, 400)
+  graph.addVertex('Outside', 302, 483)
+  graph.addVertex('Bookstore_Out', 110, 483)
+  graph.addVertex('Tech Room_Out', 1030, 235)
+  graph.addVertex('Multi Purpose_Out', 993, 235)
+  graph.addVertex('Orientation Room_Out', 881, 235)
+  graph.addVertex('Nursing Lab_Out', 736,235)
+  graph.addVertex('Lobby Area_Right', 569,408)
+  graph.addVertex('Lobby Area_Left', 302,408)
+  graph.addVertex('Clinic_Out', 302, 382)
+  graph.addVertex('SOCIP_Out', 832, 235)
+  graph.addVertex('Student Council_Out', 675, 235)
+  graph.addVertex('Back Lounge', 569, 235)
+  
   
 
   
-  graph.addEdge('elevator', '1'); //elevator to lobby
-  graph.addEdge('elevator', '2'); //elevator to stairs
-  graph.addEdge('elevator', '3'); //elevator to lobby
-  graph.addEdge('fire-exit-elevator', '2'); //fire-exit edge
-  graph.addEdge('canteen', '4'); //canteen to near canteen exit
-  graph.addEdge('Student lounge 1', '5');  //student lounge to canteen entrancce
-  graph.addEdge('Student lounge 1', '3'); //student lounge to elevator
-  graph.addEdge('hallway', '3'); //hallway to elevator
-  graph.addEdge('hallway', '6');  //hallway to clinic
-  graph.addEdge('clinic', '6'); //clinic edge
-  graph.addEdge('orientation room', '7'); //orientation room edge 
-  graph.addEdge('counsellor', '9'); //counsellor edge
-  graph.addEdge('multi-purpose', '10'); //multipurpose edge
-  graph.addEdge('tech serve', '11'); //techserve edge
-  graph.addEdge('student council', '7'); //council edge
-  graph.addEdge('SOCIP', '20'); //SOCIP edge
-  graph.addEdge('Registrar', '17'); //registrar edge
-  graph.addEdge('admission', 'treasury');
-  graph.addEdge('admission', '17');
-  graph.addEdge('treasury', 'admission'); //treasury to admission
-  graph.addEdge('student lounge 2', '6'); //lounge to hallway
-  graph.addEdge('student lounge 2', '18'); //lounge to waiting
-  graph.addEdge('waiting area', '18'); //waiting area edge
-  graph.addEdge('fire exit 2', '19'); //fire exit edge
-  graph.addEdge('lobby area', '14'); //entrance
-  graph.addEdge('lobby area', '1'); //lobby to elevator
-  graph.addEdge('bookstore', '16'); //bookstore edge
-
-  graph.addEdge('15', '16');
-  graph.addEdge('18', '19');
-  graph.addEdge('16', '15');
-  graph.addEdge('15', '1');
-  graph.addEdge('3', '4');
-  graph.addEdge('8', '17');
-  graph.addEdge('14', '15');
-  graph.addEdge('5', '4');
-  graph.addEdge('6', '7');
-  graph.addEdge('7', '13');
-  graph.addEdge('13', '8');
-  graph.addEdge('8', '9');
-  graph.addEdge('9', '10');
-  graph.addEdge('10', '11');
-  graph.addEdge('7', '8');
-  graph.addEdge('7', '11');
-  graph.addEdge('7', '10');
-  graph.addEdge('6', '20');
-  graph.addEdge('treasury', '8');
-  graph.addEdge('9', '20');  
-  graph.addEdge('11', '20');  //tech to SOCIP
-  graph.addEdge('11', '8');  //tech to reg room
-  graph.addEdge('6', '11');  //tech to clinic
-  graph.addEdge('hallway', '11');  //tech to hallway
-  graph.addEdge('Student Lounge 1', '11');  //tech to stud lounge 1
-  graph.addEdge('2', '3');
-  graph.addEdge('hallway', '8');  
+  
+  //DRAWS LINES FROM A NODE TO THEIR NEAREST NEIGHBOURING NODES
+  graph.addEdge('Admin_Out1', 'Admin Assist_Out');
+  graph.addEdge('Admin_Out1', 'Admin_Out2');
+  graph.addEdge('Admin_Out3', 'Admin_Out2');
+  graph.addEdge('Admin_Out3', 'Multi Purpose_Out');
+  graph.addEdge('Admin_Out3', 'Orientation Room_Out');
+  graph.addEdge('Admin Office_Out', 'Admin Assist_Out');
+  graph.addEdge('Admin Assist', 'Admin Assist_Out');
+  graph.addEdge('Admin Office_Out', 'Admin Office');
+  graph.addEdge('Elevator Lobby', 'Student Lounge 2');
+  graph.addEdge('Elevator Lobby', 'Lobby Area_Left');
+  graph.addEdge('Canteen', 'Student Lounge 2');
+  graph.addEdge('Clinic', 'Clinic_Out');
+  graph.addEdge('Lobby Area_Left', 'Clinic_Out');
+  graph.addEdge('Elevator Lobby', 'Clinic_Out');
+  graph.addEdge('Lobby Area_Left', 'Lobby Area');
+  graph.addEdge('Bookstore_Out', 'Book Store');
+  graph.addEdge('Bookstore_Out', 'Outside');
+  graph.addEdge('Waiting Area', 'Lobby Area_Right');
+  graph.addEdge('Waiting Area', 'Registrar');
+  graph.addEdge('Waiting Area', 'Treasury');
+  graph.addEdge('Waiting Area', 'Admission');
+  graph.addEdge('Lobby Area_Left', 'Outside');
+  graph.addEdge('Lobby Area_Right', 'Student Lounge 1');
+  graph.addEdge('Lobby Area_Right', 'Lobby Area');
+  graph.addEdge('Nursing Lab', 'Nursing Lab_Out');
+  graph.addEdge('Tech Room', 'Tech Room_Out');
+  graph.addEdge('Back Lounge', 'Student Lounge 1');
+  graph.addEdge('Hallway', 'Student Lounge 2');
+  graph.addEdge('Hallway', 'Back Lounge');
+  graph.addEdge('Student Council_Out', 'Nursing Lab_Out');
+  graph.addEdge('Student Council_Out', 'Back Lounge');
+  graph.addEdge('Student Council_Out', 'Student Council');
+  graph.addEdge('SOCIP_Out', 'SOCIP');
+  graph.addEdge('SOCIP_Out', 'Nursing Lab_Out');
+  graph.addEdge('SOCIP_Out', 'Orientation Room_Out');
+  graph.addEdge('Tech Room_Out', 'Multi Purpose_Out');
+  graph.addEdge('Multi Purpose', 'Multi Purpose_Out');
+  graph.addEdge('Orientation Room', 'Orientation Room_Out');
+  graph.addEdge('Multi Purpose_Out', 'Orientation Room_Out');
+  graph.addEdge('Treasury', 'Admission');
+  graph.addEdge('Registrar', 'Admission');
+  
 
   const shortestPath = graph.bfs(startVertex, endVertex);
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
+
   // Clear the canvas
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //
   
   
   if (shortestPath) {
